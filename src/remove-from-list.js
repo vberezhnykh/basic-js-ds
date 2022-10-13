307b404dab63dcd33b4f4e426c9e3f6542899b6d
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2022-10-13 21:27:21 +0300

    feat: add solution to task remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  let node = l,
+      previous = null;
+  
+  while (l) {
+    if (l.value === k) {
+      if (previous === null) {
+        node = l = l.next;
+        continue;
+      } else {
+        previous.next = l.next;
+        l = l.next;
+        continue;
+      }
+    }
+    previous = l;
+    l = l.next;
+  }
+  return node;
 }
 
 module.exports = {
